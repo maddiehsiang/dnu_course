3d107d672f2aa4db1cc02760e1cac0e907dffcb4
m <mmyros@gmail.com>
2021-05-10 12:21:19 -0400

    solutions

---
+++ tutorials/solutions/spikes_tutorial_Solution_0c7c37d5.py
@@ -0,0 +1,4 @@
+# Whole raster for one neuron, no loop
+plt.scatter(x=spike_times_of_interest,
+            y=np.ones(spike_times_of_interest.shape[0]),
+            marker='|');
---
+++ tutorials/solutions/spikes_tutorial_Solution_bf4d5a1c.py
@@ -0,0 +1,4 @@
+# For all neurons now
+plt.scatter(x=spike_times_all_neurons,
+            y=neuron_ids,
+            marker='|', color='black', alpha=.7);
